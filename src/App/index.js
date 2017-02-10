39fc887bf199739af629870aa0bfc3e97f6b37c2
Fredrik Andersson <f.anderzon@gmail.com>
2017-02-10 01:44:19 +0100

    Moved `App` module into it’s own folder

---
+++ src/App/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import InfiniteList from '../InfiniteList';
+import InfinitePaged from '../InfinitePaged';
+
+import logo from './logo.svg';
+import './index.css';
+import ListItem from './ListItem';
+import api from './api';
+import sideEffects from './sideEffects';
+import reducer from './reducer';
+import StateContainer from './StateContainer';
+
+const App = ({pages, items, dispatch}) => {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Welcome to React</h2>
+      </div>
+      <div className="App-list">
+        <InfinitePaged
+          pages={pages}
+          pageItems={items}
+          itemHeight={170}
+          Component={ListItem}
+          dispatch={dispatch}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default StateContainer(reducer)(sideEffects)(App);
