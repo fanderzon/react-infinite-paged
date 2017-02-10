d05c60e4d4676c49af39ae3d4f8ebbd15cf4d575
Fredrik Andersson <f.anderzon@gmail.com>
2017-02-10 01:45:08 +0100

    Added `App` dependencies to the App folder

---
+++ src/App/api.js
@@ -0,0 +1,13 @@
+let items = Array.apply(null, Array(1000)).map((item, i) => ({ content: i}));
+
+const api = {
+  fetch({offset,limit}) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(items.slice(offset, (offset + limit)));
+      }, 2000);
+    });
+  }
+};
+
+export default api;
